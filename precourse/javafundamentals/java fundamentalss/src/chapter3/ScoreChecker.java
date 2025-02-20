fd8758c00b27f5692bc5544ea2e151c4c1e8f90a
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-02-20 22:47:54 +0100

    Added switch logic

--- precourse/javafundamentals/java fundamentalss/src/chapter3/ScoreChecker.java
+++ precourse/javafundamentals/java fundamentalss/src/chapter3/ScoreChecker.java
@@ -8,6 +8,7 @@ public class ScoreChecker {
         Scanner scanner = new Scanner(System.in);
         char grade;
         int score;
+        String message="";
 
         System.out.println("Please enter your score and we will calculate your grade accordingly.");
         score = scanner.nextInt();
@@ -24,6 +25,24 @@ public class ScoreChecker {
         }
 
         System.out.println("Based on your score, your grade is "+grade+".");
+
+        switch (grade){
+            case 'A':
+                message="Great job!";
+                break;
+            case 'B':
+                message="Nice one! It is important that we value your effort.";
+                break;
+            case 'C':
+                message="You barely passed it, please try harder next time!";
+                break;
+            case 'D':
+                message="Sadly, this is a no pass. Better effort for the upcoming tests please.";
+                break;
+        }
+
+        System.out.println(message);
+
 
 
     }
