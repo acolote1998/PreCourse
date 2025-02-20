9de1f744aa930aea5ecc7369295b12e527b609fd
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-02-20 21:12:56 +0100

    Added result of the exercise

--- precourse/javafundamentals/java fundamentalss/src/exercise/SentenceGenerator.java
+++ precourse/javafundamentals/java fundamentalss/src/exercise/SentenceGenerator.java
@@ -18,11 +18,19 @@ public class SentenceGenerator {
 
         System.out.println("Please write a season of the year");
         String season = scanner.next();
+
         System.out.println("Thank you.");
+
         System.out.println("Please write an integer.");
         int number = scanner.nextInt();
+
         System.out.println("Thank you.");
+
         System.out.println("As a last thing, would you please write and adjective for me?.");
         String adjective = scanner.next();
+
+        System.out.println("I appreciate it.");
+
+        System.out.println("It seems that on a "+adjective+" "+season+" day, I drink a minimum of "+number+" cups of coffee.");
     }
 }
