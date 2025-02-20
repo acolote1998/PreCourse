f022293996042410d0e6043d01ea34419d23e9b9
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-02-20 21:43:26 +0100

    Base variables

--- precourse/javafundamentals/java fundamentalss/src/chapter3/SalaryCalculator.java
+++ precourse/javafundamentals/java fundamentalss/src/chapter3/SalaryCalculator.java
@@ -1,9 +1,15 @@
 package chapter3;
 
+import java.util.Scanner;
+
 public class SalaryCalculator {
     public static void main(String arg[]){
 //If
 // All salespeople get a payment of $1000 a week
 // Salespeople who exceed 10 sales get an additional bonus of 250
+        int baseSalary = 10000;
+        int bonus = 250;
+        Scanner scanner = new Scanner(System.in);
+
     }
 }
