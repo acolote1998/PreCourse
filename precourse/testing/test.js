c3608e291f775b10ca7cc58fe39c64b3680eb3ee
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-02-20 18:23:49 +0100

    used prettier

--- precourse/testing/test.js
+++ precourse/testing/test.js
@@ -1,30 +1,26 @@
-const assert = require('assert');
+const assert = require("assert");
 
-describe ('greet function', ()=> {
-    // arrange  => makes sure that what we will test is going to be available and ready in the code
-    const greeter = require('./index.js');
+describe("greet function", () => {
+  // arrange  => makes sure that what we will test is going to be available and ready in the code
+  const greeter = require("./index.js");
 
-    it ('should greet Marcus properly', () => {
-
+  it("should greet Marcus properly", () => {
     // act => where we write the test. For simplicity, only one thing per test
 
-    const actualResult = greeter.greet('Marcus');
+    const actualResult = greeter.greet("Marcus");
 
     // assert. check if what we want, is what we got as result from the greet function
 
-    assert.equal('Welcome to SALT, Marcus', actualResult);
+    assert.equal("Welcome to SALT, Marcus", actualResult);
+  });
 
-    });
+  it("should greet Eliza properly", () => {
+    // act => where we write the test. For simplicity, only one thing per test
 
-    it ('should greet Eliza properly', () => {
+    const actualResult = greeter.greet("Eliza");
 
-        // act => where we write the test. For simplicity, only one thing per test
-    
-        const actualResult = greeter.greet('Eliza');
-    
-        // assert. check if what we want, is what we got as result from the greet function
-    
-        assert.equal('Welcome to SALT, Eliza', actualResult);
-    
-        });
+    // assert. check if what we want, is what we got as result from the greet function
+
+    assert.equal("Welcome to SALT, Eliza", actualResult);
+  });
 });
