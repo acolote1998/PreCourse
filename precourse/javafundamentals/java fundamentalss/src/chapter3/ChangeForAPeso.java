09f6388693eb003f72a2847eecc32f185633e17c
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-02-20 23:06:18 +0100

    Last chapter 3 exercise title

---
+++ precourse/javafundamentals/java fundamentalss/src/chapter3/ChangeForAPeso.java
@@ -0,0 +1,12 @@
+package chapter3;
+
+public class ChangeForAPeso {
+    private static void main(String args[]){
+        //The objective of the game is to enter enough change
+        //to exactly 1 peso. Create a program that asks a user to
+        //enter quantities for the follwing coins:
+        //1 cent, 5 cents, 25 cents, 50 cents
+        //If it is exactly 1 peso, they win. If it is more tell them
+        //by how much, and if it is less than 1 peso, tell them by how much.
+    }
+}
