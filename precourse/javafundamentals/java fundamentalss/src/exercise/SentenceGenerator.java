0bb6b64f1df7b6c717cf52c02f26af434146dbe8
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-02-20 21:06:32 +0100

    Can write season of the year.

--- precourse/javafundamentals/java fundamentalss/src/exercise/SentenceGenerator.java
+++ precourse/javafundamentals/java fundamentalss/src/exercise/SentenceGenerator.java
@@ -7,13 +7,17 @@ public class SentenceGenerator {
 
 
         //Taks:
-        //Create a program that asks a user for a searon of the year
+        //Create a program that asks a user for a season of the year
         //then a whole number, then an adjective. Use input to complete the
         // sentence below then output the result:
 
         //"On a [adjective] [season of the year] day, I drink a minimum
-        // of [wholle number] cups of coffee."
+        // of [whole number] cups of coffee."
 
         Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please write a season of the year");
+        String season = scanner.next();
+
     }
 }
