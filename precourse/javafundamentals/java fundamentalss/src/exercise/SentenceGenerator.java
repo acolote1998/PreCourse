a131b1e071dc92377bdc39e83ea6412975670826
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-02-20 21:02:58 +0100

    Excercise premise

--- precourse/javafundamentals/java fundamentalss/src/exercise/SentenceGenerator.java
+++ precourse/javafundamentals/java fundamentalss/src/exercise/SentenceGenerator.java
@@ -3,5 +3,13 @@ package exercise;
 public class SentenceGenerator {
     public static void main(String arg[]){
 
+
+        //Taks:
+        //Create a program that asks a user for a searon of the year
+        //then a whole number, then an adjective. Use input to complete the
+        // sentence below then output the result:
+
+        //"On a [adjective] [season of the year] day, I drink a minimum
+        // of [wholle number] cups of coffee."
     }
 }
