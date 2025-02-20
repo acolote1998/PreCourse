0d7ff25c7bb26bc1dcd3daf7d5faa46abf7216f4
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-02-20 22:04:44 +0100

    Getting the salary input

--- precourse/javafundamentals/java fundamentalss/src/chapter3/LoanQualifier.java
+++ precourse/javafundamentals/java fundamentalss/src/chapter3/LoanQualifier.java
@@ -4,13 +4,16 @@ import java.util.Scanner;
 
 public class LoanQualifier {
     public static void main(String arg[]){
-        //To qualify for a loan, a person must take at least
+        // To qualify for a loan, a person must take at least
         // 30 000 and have been working at their current job
         // for at least 2 years.
 
         int salaryRequeriment = 30000;
         int monthsWorking = 24;
         Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please write your salary:");
+        int usersSalary = scanner.nextInt();
 
     }
 }
