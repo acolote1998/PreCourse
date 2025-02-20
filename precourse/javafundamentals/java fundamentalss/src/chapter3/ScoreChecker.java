6884c71d1dd4150d5ce058ec627554f800a3f342
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-02-20 22:23:27 +0100

    Exercise with conditional logic

--- precourse/javafundamentals/java fundamentalss/src/chapter3/ScoreChecker.java
+++ precourse/javafundamentals/java fundamentalss/src/chapter3/ScoreChecker.java
@@ -12,6 +12,18 @@ public class ScoreChecker {
         System.out.println("Please enter your score and we will calculate your grade accordingly.");
         score = scanner.nextInt();
 
+        if (score>90){
+            grade = 'A';
+        } else if(score>80){
+
+                grade ='B';
+            } else if(score>70){
+            grade = 'C';
+        }else{
+            grade = 'D';
+        }
+
+        System.out.println("Based on your score, your grade is "+grade+".");
 
 
     }
