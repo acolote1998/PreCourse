d788f99abb9504dc3b77c0dbd7db518d41c4d5bc
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-02-20 22:16:46 +0100

    Initialising variables

--- precourse/javafundamentals/java fundamentalss/src/chapter3/ScoreChecker.java
+++ precourse/javafundamentals/java fundamentalss/src/chapter3/ScoreChecker.java
@@ -1,7 +1,13 @@
 package chapter3;
 
+import java.util.Scanner;
+
 public class ScoreChecker {
     public static void main(String args[]){
+
+        Scanner scanner = new Scanner(System.in);
+        char grade;
+        int score;
 
     }
 }
