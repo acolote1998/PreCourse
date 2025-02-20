dd74794661b154bf2b1ff59f372610d2b30efae9
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-02-20 23:07:40 +0100

    Initializing variables

--- precourse/javafundamentals/java fundamentalss/src/chapter3/ChangeForAPeso.java
+++ precourse/javafundamentals/java fundamentalss/src/chapter3/ChangeForAPeso.java
@@ -4,9 +4,13 @@ public class ChangeForAPeso {
     private static void main(String args[]){
         //The objective of the game is to enter enough change
         //to exactly 1 peso. Create a program that asks a user to
-        //enter quantities for the follwing coins:
+        //enter quantities for the following coins:
         //1 cent, 5 cents, 25 cents, 50 cents
         //If it is exactly 1 peso, they win. If it is more tell them
         //by how much, and if it is less than 1 peso, tell them by how much.
+
+        int oneCent, fiveCents, twentyFiveCents, fiftyCents;
+        int goal = 100;
+
     }
 }
