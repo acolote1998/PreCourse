ae4efbc8bb5f6e342e7dc9c9e369f7ffdeb258b3
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-02-20 21:41:08 +0100

    Exercise description

--- precourse/javafundamentals/java fundamentalss/src/chapter3/SalaryCalculator.java
+++ precourse/javafundamentals/java fundamentalss/src/chapter3/SalaryCalculator.java
@@ -2,6 +2,8 @@ package chapter3;
 
 public class SalaryCalculator {
     public static void main(String arg[]){
-
+//If
+// All salespeople get a payment of $1000 a week
+// Salespeople who exceed 10 sales get an additional bonus of 250
     }
 }
