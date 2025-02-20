a48ec3a1453477f09084bb3b52acd30d5053a2ea
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-02-20 22:01:53 +0100

    nested conditionals exercise title

---
+++ precourse/javafundamentals/java fundamentalss/src/chapter3/LoanQualifier.java
@@ -0,0 +1,9 @@
+package chapter3;
+
+public class LoanQualifier {
+    public static void main(String arg[]){
+        //To qualify for a loan, a person must take at least
+        // 30 000 and have been working at their current job
+        // for at least 2 years.
+    }
+}
