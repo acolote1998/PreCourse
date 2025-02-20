d0c04edad4056c848946443a085d8d51ae1350a4
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-02-20 22:12:19 +0100

    Conditional logic added

--- precourse/javafundamentals/java fundamentalss/src/chapter3/LoanQualifier.java
+++ precourse/javafundamentals/java fundamentalss/src/chapter3/LoanQualifier.java
@@ -20,7 +20,16 @@ public class LoanQualifier {
 
         scanner.close();
 
+        System.out.println("Thank you. We will proceed to calculate your chances of a loan.");
 
-
+        if (usersSalary>=salaryRequeriment){
+            if (usersWorkedMonths>=monthsWorking){
+                System.out.println("We see that you meet the requirements for the loan. Would you be available for a meeting tomorrow at 09:15?");
+            }else{
+                System.out.println("Sorry, you must work "+ (monthsWorking-usersWorkedMonths) +" more month/s to qualify for this loan.");
+            }
+        }else {
+            System.out.println("Sorry, you must earn at least 30 000 SEK to qualify for this loan.");
+        }
     }
 }
