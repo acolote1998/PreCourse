4d684c3ed2ba47d741f166eb5dc943b767946933
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-02-20 17:56:04 +0100

    adding initial verification

--- precourse/testing/index.js
+++ precourse/testing/index.js
@@ -1,3 +1,11 @@
 function greet(namePassedIn) {
     return 'Welcome to SALT, ' + namePassedIn;
   }
+
+  const greeting = greet('Marcus');
+  if(greeting === 'Welcome to SALT, Marcus') {
+    console.log('IT WORKS');
+  }
+  else {
+    console.log('IT FAILS');
+  }
