bcf7003107b27accf517307ee1ad171066780f27
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-02-21 16:34:33 +0100

    Method parsing number to string

--- precourse/javafundamentals/java fundamentalss/src/chapter6/Month.java
+++ precourse/javafundamentals/java fundamentalss/src/chapter6/Month.java
@@ -2,7 +2,34 @@ package chapter6;
 
 public class Month {
     public static String getMonth(int month) {
-        return "";
+        switch (month) {
+            case 1:
+                return "January";
+            case 2:
+                return "February";
+            case 3:
+                return "March";
+            case 4:
+                return "April";
+            case 5:
+                return "May";
+            case 6:
+                return "June";
+            case 7:
+                return "July";
+            case 8:
+                return "August";
+            case 9:
+                return "September";
+            case 10:
+                return "October";
+            case 11:
+                return "November";
+            case 12:
+                return "December";
+            default:
+                return "Not a month";
+        }
     }
 
     public static int getMonth(String month) {
