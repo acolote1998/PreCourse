194eccfb8d2cbb77d693347f96f9fa8dcf0ecd11
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-02-21 16:42:15 +0100

    Phone bill exercise statement

---
+++ precourse/javafundamentals/java fundamentalss/src/chapter6/PhoneBillCalculator.java
@@ -0,0 +1,13 @@
+package chapter6;
+
+public class PhoneBillCalculator {
+    //Phone bill exercise
+    //A phone bill should have an id, base cost, number of alloted minutes
+    // number of minutes used. One should also be able to calculate the average,
+    // tax and total on a phone bill, also be able to print an itemized bill.
+
+    //Include the following constructors: default, id-only, all fields. No matter which
+    // constructor is used, all fields should be set.
+    // Create a class that instantiates a phone bill and prints the itemized bill
+    
+}
