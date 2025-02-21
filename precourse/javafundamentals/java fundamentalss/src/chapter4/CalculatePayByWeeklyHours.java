f698eef30b0e6ab22a1153653257b4b201edb376
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-02-21 11:12:06 +0100

    exercise statement

--- precourse/javafundamentals/java fundamentalss/src/chapter4/CalculatePayByWeeklyHours.java
+++ precourse/javafundamentals/java fundamentalss/src/chapter4/CalculatePayByWeeklyHours.java
@@ -1,4 +1,11 @@
 package chapter4;
 
 public class CalculatePayByWeeklyHours {
+    public static void main(String args[]){
+        //Each store employee makes $15 an hour
+        // Write a program that allows a manager to enter
+        // The number of weekly hours worked for each employee
+        // Calculate their pay
+        // Do not allow overtime
+    }
 }
