7db6bd9364eb4b7c731f62af7bf6174f9566d47a
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-02-21 12:00:56 +0100

    for loop exercise statement

---
+++ precourse/javafundamentals/java fundamentalss/src/chapter4/forLoopExercise.java
@@ -0,0 +1,8 @@
+package chapter4;
+
+public class forLoopExercise {
+    public static void main(String args[]){
+        //Write a for loop that allows a cashier to scan a given
+        // number of items, and sum up the total cost
+    }
+}
