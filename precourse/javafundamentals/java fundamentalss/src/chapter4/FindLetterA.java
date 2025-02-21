467f02eb58b95eba88c058481ea24f00a528dc1c
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-02-21 12:17:02 +0100

    initialising variables

--- precourse/javafundamentals/java fundamentalss/src/chapter4/FindLetterA.java
+++ precourse/javafundamentals/java fundamentalss/src/chapter4/FindLetterA.java
@@ -1,8 +1,14 @@
 package chapter4;
 
+import java.util.Scanner;
+
 public class FindLetterA {
     public static void main(String args[]){
         // Loop break
         // Search a string to determine if it contains the letter 'A'
+        boolean letterFound = false;
+        String text = "";
+        Scanner scanner = new Scanner(System.in);
+
     }
 }
