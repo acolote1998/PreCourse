fd81b4b65d2876836bd83719d1182b1e5354c022
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-02-21 12:15:43 +0100

    Exercise statement

---
+++ precourse/javafundamentals/java fundamentalss/src/chapter4/FindLetterA.java
@@ -0,0 +1,8 @@
+package chapter4;
+
+public class FindLetterA {
+    public static void main(String args[]){
+        // Loop break
+        // Search a string to determine if it contains the letter 'A'
+    }
+}
