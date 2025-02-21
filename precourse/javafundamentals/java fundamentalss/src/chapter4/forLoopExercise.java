56cffb0116f82f79b55d0f2232cc5a756df7418e
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-02-21 12:08:22 +0100

    showing total to pay

--- precourse/javafundamentals/java fundamentalss/src/chapter4/forLoopExercise.java
+++ precourse/javafundamentals/java fundamentalss/src/chapter4/forLoopExercise.java
@@ -16,11 +16,13 @@ public class forLoopExercise {
         quantity = scanner.nextInt();
 
         for (int i=0;i<quantity;i++){
-        System.out.println("Please introduce the price of the item number"+(i+1));
+        System.out.println("Please introduce the price of the item number "+(i+1));
         itemPrice=scanner.nextDouble();
         totalPrice=totalPrice+itemPrice;
         }
 
+        System.out.println("Thank you for your cooperation");
+        System.out.println("The total to pay is "+totalPrice+" SEK");
 
     }
 }
