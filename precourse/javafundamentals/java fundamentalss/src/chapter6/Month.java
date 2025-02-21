9c2ced3ee9754e52340ec71956c68225fd293439
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-02-21 16:35:38 +0100

    parsing text to month number

--- precourse/javafundamentals/java fundamentalss/src/chapter6/Month.java
+++ precourse/javafundamentals/java fundamentalss/src/chapter6/Month.java
@@ -33,7 +33,35 @@ public class Month {
     }
 
     public static int getMonth(String month) {
-        return 0;
+        switch (month) {
+            case "January":
+                return 1;
+            case "February":
+                return 2;
+            case "March":
+                return 3;
+            case "April":
+                return 4;
+            case "May":
+                return 5;
+            case "June":
+                return 6;
+            case "July":
+                return 7;
+            case "August":
+                return 8;
+            case "September":
+                return 9;
+            case "October":
+                return 10;
+            case "November":
+                return 11;
+            case "December":
+                return 12;
+            default:
+                return -1; // Return -1 if the month is invalid
+        }
     }
+
 
 }
