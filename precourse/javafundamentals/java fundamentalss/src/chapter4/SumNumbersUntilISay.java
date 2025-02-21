18bf1a0aa90fb417a141797f031ba174c04d1ca4
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-02-21 11:49:55 +0100

    Initialising variables

---
+++ precourse/javafundamentals/java fundamentalss/src/chapter4/SumNumbersUntilISay.java
@@ -0,0 +1,17 @@
+package chapter4;
+
+import java.util.Scanner;
+
+public class SumNumbersUntilISay {
+    public static void main(String args[]){
+
+        // Write a program that allows a user to enter
+        // two numbers, and then sums up the two numbers.
+        // The user should be able to repeat this action
+        // until they indicate they are done.
+
+        Scanner scanner = new Scanner(System.in);
+        int numberOne, numberTwo;
+        boolean repeat;
+    }
+}
