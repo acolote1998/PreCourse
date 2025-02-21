d4fd1a5b84ed2218d7ee89cb88abe0d14bbcf985
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-02-21 12:02:05 +0100

    initialising variables

--- precourse/javafundamentals/java fundamentalss/src/chapter4/forLoopExercise.java
+++ precourse/javafundamentals/java fundamentalss/src/chapter4/forLoopExercise.java
@@ -1,8 +1,14 @@
 package chapter4;
 
+import java.util.Scanner;
+
 public class forLoopExercise {
     public static void main(String args[]){
         //Write a for loop that allows a cashier to scan a given
         // number of items, and sum up the total cost
+
+        Scanner scanner = new Scanner(System.in);
+        int quantity;
+        double totalPrice, itemPrice;
     }
 }
