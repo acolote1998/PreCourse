7486213eac21f9faaa7d97b13ebc00aed41366f8
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-02-21 20:36:38 +0100

    exercise instructions

--- precourse/javafundamentals/java fundamentalss/src/chapter7/ArrayPractice.java
+++ precourse/javafundamentals/java fundamentalss/src/chapter7/ArrayPractice.java
@@ -1,4 +1,11 @@
 package chapter7;
 
 public class ArrayPractice {
+    public static void main(String args[]) {
+        //Create a Lottery Quick
+        // Pick application that
+        // will generate a lottery
+        // ticket with 6 random numbers
+        // between 1 and 69
+    }
 }
