8d6496c887e992741e0b647243849360260328f5
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-02-21 20:42:28 +0100

    exercise done

--- precourse/javafundamentals/java fundamentalss/src/chapter7/ArrayPractice.java
+++ precourse/javafundamentals/java fundamentalss/src/chapter7/ArrayPractice.java
@@ -1,11 +1,23 @@
 package chapter7;
 
+import java.util.Random;
+
 public class ArrayPractice {
     public static void main(String args[]) {
         //Create a Lottery Quick
         // Pick application that
         // will generate a lottery
         // ticket with 6 random numbers
-        // between 1 and 69
+        // between 0 and 69
+
+        Random randomNumber = new Random();
+
+        int[] loteryRandomNumbers = {randomNumber.nextInt(70), randomNumber.nextInt(70), randomNumber.nextInt(70), randomNumber.nextInt(70), randomNumber.nextInt(70), randomNumber.nextInt(70)};
+
+        for (int i = 0; i < loteryRandomNumbers.length; i++) {
+            System.out.println("Number " + (i + 1) + " " + loteryRandomNumbers[i]);
+        }
+
     }
+
 }
