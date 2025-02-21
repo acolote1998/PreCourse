7a26fd98d8997f3506472f7ac3b1889c97beefbe
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-02-21 12:32:53 +0100

    Initialising variables

--- precourse/javafundamentals/java fundamentalss/src/chapter4/nestedLoops.java
+++ precourse/javafundamentals/java fundamentalss/src/chapter4/nestedLoops.java
@@ -2,6 +2,11 @@ package chapter4;
 
 public class nestedLoops {
     public static void main(String args[]){
+// Find the average teste score for each student in the class.
+        // There are 24 students and 4 tests
+
+        int numberOfStudents = 24;
+        int numberOfTests = 4;
 
     }
 }
