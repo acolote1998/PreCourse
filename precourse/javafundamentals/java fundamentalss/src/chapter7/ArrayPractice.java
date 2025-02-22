6d877bd1cc67a1295a27d4346827e6dab936abb1
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-02-22 09:26:59 +0100

    practice with enhanced loop. now it does not allow duplicates

--- precourse/javafundamentals/java fundamentalss/src/chapter7/ArrayPractice.java
+++ precourse/javafundamentals/java fundamentalss/src/chapter7/ArrayPractice.java
@@ -3,21 +3,47 @@ package chapter7;
 import java.util.Random;
 
 public class ArrayPractice {
+
+    static Random randomNumber = new Random();
+    static int amountOfTickets = 6;
+    static int maximumTicketNumber = 69;
+
+
     public static void main(String args[]) {
         //Create a Lottery Quick
         // Pick application that
         // will generate a lottery
         // ticket with 6 random numbers
-        // between 0 and 69
+        // between 1 and 69
 
-        Random randomNumber = new Random();
+        int[] ticket = generateNumbers();
 
-        int[] loteryRandomNumbers = {randomNumber.nextInt(70), randomNumber.nextInt(70), randomNumber.nextInt(70), randomNumber.nextInt(70), randomNumber.nextInt(70), randomNumber.nextInt(70)};
-
-        for (int i = 0; i < loteryRandomNumbers.length; i++) {
-            System.out.println("Number " + (i + 1) + " " + loteryRandomNumbers[i]);
+        for (int i = 0; i < amountOfTickets; i++) {
+            System.out.print("|" + ticket[i] + "|");
         }
+    }
 
+    public static int[] generateNumbers() {
+        int[] loteryRandomNumbers = new int[amountOfTickets];
+
+        for (int i = 0; i < amountOfTickets; i++) {
+            int newRandomNumber;
+            do {
+                newRandomNumber = randomNumber.nextInt(maximumTicketNumber) + 1; // Generate new number
+            } while (search(loteryRandomNumbers, newRandomNumber));
+
+            loteryRandomNumbers[i] = newRandomNumber; // Assign only after confirming uniqueness
+        }
+        return loteryRandomNumbers;
+    }
+
+    public static boolean search(int[] array, int numberToSearchFor) {
+        for (int value : array) {
+            if (value == numberToSearchFor) {
+                return true;
+            }
+        }
+        return false;
     }
 
 }
