5a320e33ebe3e088a0118208839dc03101fa5ed8
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-02-22 10:36:15 +0100

    initialising array with input

--- precourse/javafundamentals/java fundamentalss/src/chapter7/Grades.java
+++ precourse/javafundamentals/java fundamentalss/src/chapter7/Grades.java
@@ -1,9 +1,20 @@
 package chapter7;
 
+import java.util.Scanner;
+
 /*
 Create a program that allows the user to enter any
 number of grades and provides them with their
 average score, as well as the highest and lowest scores.
  */
 public class Grades {
+
+    private static int grades[];
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String args[]) {
+
+        System.out.println("How many grades would you like to enter?");
+        grades = new int[scanner.nextInt()];
+    }
 }
