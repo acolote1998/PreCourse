915f8721f675db6f488184f7af24010174a67fba
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-02-22 11:34:05 +0100

    Chapter 8 Exercise - Counting words

---
+++ precourse/javafundamentals/java fundamentalss/src/chapter8/CountingWords.java
@@ -0,0 +1,15 @@
+package chapter8;
+
+/*
+Write a method that counts
+the number of words in a
+string and prints them
+individually on a
+new line.
+ */
+
+public class CountingWords {
+    public static void main(String args[]) {
+
+    }
+}
