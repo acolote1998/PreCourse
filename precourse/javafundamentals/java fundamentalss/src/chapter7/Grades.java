bd218f03b2d7dbc1adcb4b6091eab0673d88ae02
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-02-22 10:33:48 +0100

    Chapter 7 - Tricks for arrays exercise

---
+++ precourse/javafundamentals/java fundamentalss/src/chapter7/Grades.java
@@ -0,0 +1,9 @@
+package chapter7;
+
+/*
+Create a program that allows the user to enter any
+number of grades and provides them with their
+average score, as well as the highest and lowest scores.
+ */
+public class Grades {
+}
