6a3a30bb381a8d6f5ffd4cf9c6c116884a472ac6
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-02-22 10:52:36 +0100

    Chapter 7 final assignment title

---
+++ precourse/javafundamentals/java fundamentalss/src/chapter7/DaysOfTheWeek.java
@@ -0,0 +1,15 @@
+package chapter7;
+
+/*
+Make an array that holds the textual values of the days of the week
+
+Have the user input a number corresponding to the day of the week.
+Assume that the week starts on monday
+
+Your program should output the String that represents the day of the week
+
+Example: User inputs the number 1, your program should print "Monday"
+ */
+
+public class DaysOfTheWeek {
+}
