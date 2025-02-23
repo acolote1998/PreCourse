6a7c92c70f11cb1f7e2b05dd8c9c8df0acecea2e
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-02-23 20:50:33 +0100

    commenting code

--- precourse/javafundamentals/java fundamentalss/src/chapter10/Zoo.java
+++ precourse/javafundamentals/java fundamentalss/src/chapter10/Zoo.java
@@ -2,10 +2,11 @@ package chapter10;
 
 public class Zoo {
     public static void main(String args[]) {
+     /*
         Dog rocky = new Dog();
         rocky.fetch();
         rocky.makeSound();
-
+    */
         Animal sasha = new Dog();
         sasha.makeSound();
     }
