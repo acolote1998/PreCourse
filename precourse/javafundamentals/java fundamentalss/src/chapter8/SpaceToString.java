7d1f171b4669796c5a49cbca803cb988edd31151
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-02-23 15:56:09 +0100

    Creating base method

--- precourse/javafundamentals/java fundamentalss/src/chapter8/SpaceToString.java
+++ precourse/javafundamentals/java fundamentalss/src/chapter8/SpaceToString.java
@@ -7,4 +7,11 @@ with no spaces. Each new words begins with a capital letter.
  */
 
 public class SpaceToString {
+    public static void main(String args[]) {
+
+    }
+
+    public static String jumbledToSpaced(String string) {
+        return null;
+    }
 }
