71278d552cc5a1423ba37940f49604b1cc535bc7
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-02-23 21:53:35 +0100

    Creating a book

--- precourse/javafundamentals/java fundamentalss/src/chapter11/Customer.java
+++ precourse/javafundamentals/java fundamentalss/src/chapter11/Customer.java
@@ -2,6 +2,9 @@ package chapter11;
 
 public class Customer {
     public static void main(String args[]) {
-
+        Product book = new Book();
+        book.setPrice(9);
+        book.setColor("Red");
+        book.setName("Harry Potter");
     }
 }
