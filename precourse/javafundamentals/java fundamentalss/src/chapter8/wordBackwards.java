74182f509be07a3e89a85f022c32f2ae3e21049a
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-02-23 15:25:33 +0100

    Exercise: print a word backwards

---
+++ precourse/javafundamentals/java fundamentalss/src/chapter8/wordBackwards.java
@@ -0,0 +1,11 @@
+package chapter8;
+
+public class wordBackwards {
+    private static void main(String args[]) {
+        /*
+        Write a method that prints a given
+        String backwards. For example,
+        if given camel, it prints lemac.
+         */
+    }
+}
