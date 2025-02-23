8fe11d18b3d2330c85ea9ed0f4018a185e7709ea
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-02-23 17:12:24 +0100

    Exercise: Password Validation

---
+++ precourse/javafundamentals/java fundamentalss/src/chapter8/PasswordValidator.java
@@ -0,0 +1,16 @@
+package chapter8;
+
+/*
+
+Validate the complexity of a proposed password by assuring it meets
+these rules:
+
+At least 8 characters long
+Contain an uppercase letter
+Contain a special character
+Not containing the username
+Not containing same as the old password
+
+ */
+public class PasswordValidator {
+}
