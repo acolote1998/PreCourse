d14b4ea7a74da6880a75a6129556846482fc621c
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-02-23 15:21:06 +0100

    exercise end

--- precourse/javafundamentals/java fundamentalss/src/chapter8/CountingWords.java
+++ precourse/javafundamentals/java fundamentalss/src/chapter8/CountingWords.java
@@ -8,17 +8,25 @@ individually on a
 new line.
  */
 
+import java.util.Scanner;
+
 public class CountingWords {
 
     private static String words[];
 
     public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please write a sentence and I will give you back its words");
+        String string = scanner.nextLine();
+        printWords(words, string);
 
     }
 
-    public static void printWords(String[] array) {
+    public static void printWords(String[] array, String string) {
+        array = string.split(" ");
         for (int i = 0; i < array.length; i++) {
-            System.out.println(array[i]);
+            System.out.println((i + 1) + " " + array[i]);
         }
     }
 }
