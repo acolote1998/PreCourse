073c46caea99695f0ac02135349811519de85c7a
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-02-23 20:32:07 +0100

    TateTester class

---
+++ precourse/javafundamentals/java fundamentalss/src/chapter9/TasteTester.java
@@ -0,0 +1,23 @@
+package chapter9;
+
+/*
+
+Create a superclass called Cake that has two fields:
+Flavor and Price
+Getter and setter methods should be created for these fields
+
+Create a BirthdayCake class which inherits from Cake and has a field
+called candles. Include getter and setter method
+
+Create a WeddingCake class which inherits from Cake and has a field called
+tiers. Include getter and setter method
+
+Create a TasteTester class to test our your inheritance
+
+ */
+public class TasteTester {
+    public static void main(String args[]) {
+
+    }
+
+}
