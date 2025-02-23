2fd4eafbf7528544946119bb3310d61bb8a82276
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-02-23 22:07:04 +0100

    abstract class Animal

---
+++ precourse/javafundamentals/java fundamentalss/src/chapter11/Animal.java
@@ -0,0 +1,25 @@
+package chapter11;
+
+/*
+
+Create an abstract class called Animal, which declares an
+abstract method called makeSound() and implements a non abstract
+method called eat();
+
+Create a Pig class and a Duck class that both extend
+the Animal class
+
+Create a Farm class to test the implementations
+
+ */
+
+public abstract class Animal {
+
+    abstract public void makeSound();
+
+    public void eat() {
+        System.out.println("The animal is eating");
+    }
+
+
+}
