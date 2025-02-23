786dad723758fb94a05e740817be3eaf69e303fc
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-02-23 15:54:50 +0100

    Exercise: String to words separated by space

---
+++ precourse/javafundamentals/java fundamentalss/src/chapter8/SpaceToString.java
@@ -0,0 +1,10 @@
+package chapter8;
+
+/*
+Write a method that adds spaces to a jumbled
+String where all words are written together
+with no spaces. Each new words begins with a capital letter.
+ */
+
+public class SpaceToString {
+}
