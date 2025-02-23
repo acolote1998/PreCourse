ba1680045bc0fce2055e0cda3ed3ed1142b86882
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-02-23 15:10:45 +0100

    for loop in the method for printing words

--- precourse/javafundamentals/java fundamentalss/src/chapter8/CountingWords.java
+++ precourse/javafundamentals/java fundamentalss/src/chapter8/CountingWords.java
@@ -15,4 +15,10 @@ public class CountingWords {
     public static void main(String args[]) {
 
     }
+
+    public static void printWords(String[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+    }
 }
