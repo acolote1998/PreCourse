e80c36e5f204acf015af5d9701c4266bf1d241c1
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-02-23 20:22:14 +0100

    coded overloading method

--- precourse/javafundamentals/java fundamentalss/src/chapter9/Square.java
+++ precourse/javafundamentals/java fundamentalss/src/chapter9/Square.java
@@ -6,4 +6,12 @@ public class Square extends Rectangle {
     public double calculatePerimeter() {
         return sides * length;
     }
+
+    public void print() {
+        System.out.println("I a square");
+    }
+
+    public void print(String texto) {
+        System.out.println("I am a square," + texto);
+    }
 }
