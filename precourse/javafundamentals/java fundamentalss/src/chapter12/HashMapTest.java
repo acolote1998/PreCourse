64171fd05e33c7c4987ad240974025bd095e128e
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-02-24 20:52:45 +0100

    created hashmap and added elements to it

---
+++ precourse/javafundamentals/java fundamentalss/src/chapter12/HashMapTest.java
@@ -0,0 +1,13 @@
+package chapter12;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashMapTest {
+    public static void main(String[] args) {
+        Map fruitCalories = new HashMap();
+        fruitCalories.put("apple", 95);
+        fruitCalories.put("lemon", 10);
+        fruitCalories.put("banana", 105);
+    }
+}
