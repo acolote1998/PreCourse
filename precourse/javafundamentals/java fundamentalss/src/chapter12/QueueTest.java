d9b081d240d3fafb3343383c1e15e5dda111ac49
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-02-24 20:26:24 +0100

    adding elements to the queue

--- precourse/javafundamentals/java fundamentalss/src/chapter12/QueueTest.java
+++ precourse/javafundamentals/java fundamentalss/src/chapter12/QueueTest.java
@@ -1,6 +1,18 @@
 package chapter12;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class QueueTest {
     public static void main(String[] args) {
+
+        Queue fruit = new LinkedList();
+        fruit.add("apple");
+        fruit.add("lemon");
+        fruit.add("banana");
+        fruit.add("orange");
+        fruit.add("lemon");
+
+
     }
 }
