1c5a6df4d3a34153bc1be67800c0db357f1eecdb
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-02-24 21:00:08 +0100

    example with "listOf" that cannot change

---
+++ precourse/javafundamentals/java fundamentalss/src/chapter12/ListOfTest.java
@@ -0,0 +1,9 @@
+package chapter12;
+
+import java.util.List;
+
+public class ListOfTest {
+    public static void main(String[] args) {
+        List unchangableList = List.of("apple", "lemon", "banana");
+    }
+}
