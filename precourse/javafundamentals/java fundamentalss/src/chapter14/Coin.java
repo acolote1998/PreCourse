b0075a7a1c85345932e7a57cae249df926820127
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-02-25 07:18:22 +0100

    Coin class with sideGenerator

---
+++ precourse/javafundamentals/java fundamentalss/src/chapter14/Coin.java
@@ -0,0 +1,37 @@
+package chapter14;
+
+/*
+
+Create a Coin class which has the following:
+
+A field called side
+encapsulation
+constant variables for heads and tails
+a method called flip which randomly chooses heads or tails
+and assigns the value to side
+
+ */
+
+import java.util.Random;
+
+public class Coin {
+    public String getSide() {
+        return side;
+    }
+
+    public void setSide(String side) {
+        this.side = side;
+    }
+
+    private String side;
+    private String heads = "heads";
+    private String tails = "tails";
+
+    
+    public String randomSide() {
+        Random sideFlip = new Random();
+        if (sideFlip.nextBoolean()) {
+            return heads;
+        } else return tails;
+    }
+}
