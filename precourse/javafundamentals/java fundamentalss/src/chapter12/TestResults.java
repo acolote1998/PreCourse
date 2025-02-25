a5fc1f22e2e7f1d07a5ddb3b6d8aeb83550f011e
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-02-25 05:43:26 +0100

    Chapter 12b final exercise

---
+++ precourse/javafundamentals/java fundamentalss/src/chapter12/TestResults.java
@@ -0,0 +1,20 @@
+package chapter12;
+
+/*
+
+Students were given a pop quiz, and collectively, the did not do so well.
+As a result, the instructor decided to give them a make up exam to allow
+them to improve their scores.
+
+Given two maps of test scores, update the student's grades only if
+they did better on the make up exam.
+
+Print the final grades.
+
+ */
+
+public class TestResults {
+    public static void main(String[] args) {
+
+    }
+}
