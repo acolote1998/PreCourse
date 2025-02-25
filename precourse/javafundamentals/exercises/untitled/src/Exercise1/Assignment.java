c8be3384d609c9886b6d1a8437845afafa869963
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-02-25 23:28:17 +0100

    Exercise 1 - Reverse string

---
+++ precourse/javafundamentals/exercises/untitled/src/Exercise1/Assignment.java
@@ -0,0 +1,22 @@
+package Exercise1;
+
+/*
+
+Exercise 1: Reverse a String
+
+Write a Java method that takes a String as input and returns the reversed version of it.
+
+Example:
+Input: "hello"
+Output: "olleh"
+
+Requirements:
+Do not use built-in methods like StringBuilder.reverse().
+Use a loop to construct the reversed string manually.
+The method should have the following signature:
+public static String reverseString(String input)
+
+ */
+
+public class Assignment {
+}
