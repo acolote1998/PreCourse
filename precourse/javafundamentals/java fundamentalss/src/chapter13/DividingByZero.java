8a9e80ca0863c66b08ad922fa024fb1a12ce9094
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-02-25 06:49:29 +0100

    Final exercise exceptions chapter

---
+++ precourse/javafundamentals/java fundamentalss/src/chapter13/DividingByZero.java
@@ -0,0 +1,17 @@
+package chapter13;
+
+/*
+
+Write the following statement and excute:
+
+int c = 30 / 0;
+
+Update program to handle exception.
+
+Print a statement after division to say "Division is fun".
+This statement should be outputted no matter what.
+
+ */
+
+public class DividingByZero {
+}
