cd8ec8443ab93dbd6e75dc81baaa27b4954de1b2
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-02-25 05:45:39 +0100

    function that creates a map with the students

--- precourse/javafundamentals/java fundamentalss/src/chapter12/TestResults.java
+++ precourse/javafundamentals/java fundamentalss/src/chapter12/TestResults.java
@@ -13,8 +13,29 @@ Print the final grades.
 
  */
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class TestResults {
     public static void main(String[] args) {
 
     }
+
+    public static void initialGrades() {
+        Map<String, Integer> studentGrades = new HashMap<>();
+
+        studentGrades.put("Aki", 35);
+        studentGrades.put("Ben", 80);
+        studentGrades.put("Clara", 92);
+        studentGrades.put("Diego", 67);
+        studentGrades.put("Elena", 74);
+        studentGrades.put("Felix", 89);
+        studentGrades.put("Grace", 58);
+        studentGrades.put("Hugo", 90);
+        studentGrades.put("Isla", 77);
+        studentGrades.put("Jack", 63);
+
+        System.out.println(studentGrades);
+    }
+
 }
