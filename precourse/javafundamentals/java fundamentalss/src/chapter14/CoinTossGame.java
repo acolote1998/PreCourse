6b1f6e57e688bc4a5967d9002b5197ff0273bfdc
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-02-25 07:03:59 +0100

    final exercise

---
+++ precourse/javafundamentals/java fundamentalss/src/chapter14/CoinTossGame.java
@@ -0,0 +1,16 @@
+package chapter14;
+
+/*
+
+Create a Coin class which has the following:
+
+A field called side
+encapsulation
+constant variables for heads and tails
+a method called flip which randomly chooses heads or tails
+and assigns the value to side
+
+ */
+
+public class CoinTossGame {
+}
