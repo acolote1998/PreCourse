970e47f724a509c8fc8573b142c5aec91b0dbf2e
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-02-25 23:29:20 +0100

    method idea

--- precourse/javafundamentals/exercises/untitled/src/Exercise1/Assignment.java
+++ precourse/javafundamentals/exercises/untitled/src/Exercise1/Assignment.java
@@ -19,4 +19,11 @@ public static String reverseString(String input)
  */
 
 public class Assignment {
+    public static void main(String[] args){
+
+    }
+    public static String reverseString(String string){
+
+        return null;
+    }
 }
