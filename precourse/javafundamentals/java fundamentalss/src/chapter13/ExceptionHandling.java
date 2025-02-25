ddb2c65fbbe8a8f535b2ed5e61c55b2024c40cb9
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-02-25 06:41:34 +0100

    more exception handling

--- precourse/javafundamentals/java fundamentalss/src/chapter13/ExceptionHandling.java
+++ precourse/javafundamentals/java fundamentalss/src/chapter13/ExceptionHandling.java
@@ -1,12 +1,17 @@
 package chapter13;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class ExceptionHandling {
     public static void main(String[] args) {
 
-        createNewFile();
+        //  createNewFile();
+
+        numbersExceptionHandling();
 
     }
 
@@ -17,6 +22,23 @@ public class ExceptionHandling {
         } catch (Exception e) {
             System.out.println(e);
             e.printStackTrace();
+        }
+    }
+
+    public static void numbersExceptionHandling() {
+        File file = new File("resources/numbers.txt");
+        Scanner fileReader = null;
+        try {
+            fileReader = new Scanner(file);
+
+            while (fileReader.hasNext()) {
+                double num = fileReader.nextDouble();
+                System.out.println(num);
+            }
+        } catch (FileNotFoundException | InputMismatchException e) {
+            e.printStackTrace();
+        } finally {
+            fileReader.close();
         }
     }
 }
