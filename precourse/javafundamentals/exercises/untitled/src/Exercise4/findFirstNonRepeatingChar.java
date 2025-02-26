5e4d8c761d400be094f1d14613c529094e6ad89f
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-02-26 11:26:24 +0100

    exercise 4: find first non repeating character

---
+++ precourse/javafundamentals/exercises/untitled/src/Exercise4/findFirstNonRepeatingChar.java
@@ -0,0 +1,23 @@
+package Exercise4;
+
+/*
+
+🚀 Exercise: First Non-Repeating Character
+Problem Description:
+Write a function that finds the first non-repeating character in a given string and returns it. If all characters repeat, return null.
+
+Function Signature:
+
+public static Character firstNonRepeatingChar(String str);
+
+Input:
+A single string str (1 ≤ str.length ≤ 1000) containing only letters (a-z, A-Z).
+
+Output:
+Return the first character that appears only once in the string.
+If no unique character is found, return null.
+
+ */
+
+public class findFirstNonRepeatingChar {
+}
