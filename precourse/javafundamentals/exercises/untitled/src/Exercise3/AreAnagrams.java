03a2f4c99e6c96cc6b83bf8aa6c88911fabf9036
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-02-26 11:09:41 +0100

    exercise 3 assignment , check if 2 strings are anagrams to the other

---
+++ precourse/javafundamentals/exercises/untitled/src/Exercise3/AreAnagrams.java
@@ -0,0 +1,20 @@
+package Exercise3;
+
+/*
+
+Exercise: Anagram Checker
+Problem Description:
+Write a function that checks whether two given strings are anagrams of each other. Two words are anagrams if they contain the same characters in the same frequency, but in a different order. Ignore spaces, punctuation, and capitalization.
+
+Function Signature:
+
+public static boolean areAnagrams(String str1, String str2);
+
+Input:
+Two strings, str1 and str2, containing alphanumeric characters and possibly spaces or punctuation.
+Output:
+Return true if the two strings are anagrams, false otherwise.
+
+ */
+public class AreAnagrams {
+}
