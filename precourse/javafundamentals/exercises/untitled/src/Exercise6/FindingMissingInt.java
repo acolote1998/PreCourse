7a248d26fb6d0f11e64edc88c11238ea15ae2f42
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-02-28 10:11:50 +0100

    exercise 6 initialisation

---
+++ precourse/javafundamentals/exercises/untitled/src/Exercise6/FindingMissingInt.java
@@ -0,0 +1,26 @@
+package Exercise6;
+
+/*
+
+🚀 Exercise 6: Find the Missing Number in an Array
+Problem Description
+You are given an array of integers from 1 to n, where one number is missing. Write a function that finds and returns the missing number. You must solve it without sorting the array.
+
+Function Signature:
+
+java
+Copy
+Edit
+public static int findMissingNumber(int[] arr);
+Input:
+
+An array arr of integers containing n-1 numbers from 1 to n, where one number is missing.
+The length of the array will be between 1 and 1000.
+Output:
+
+Return the missing number.
+
+ */
+
+public class FindingMissingInt {
+}
