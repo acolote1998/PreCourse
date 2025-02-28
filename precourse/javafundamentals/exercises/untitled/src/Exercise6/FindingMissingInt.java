fb991211c730cc8396fd4775e7539b68a6474c54
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-02-28 10:18:32 +0100

    method logic

--- precourse/javafundamentals/exercises/untitled/src/Exercise6/FindingMissingInt.java
+++ precourse/javafundamentals/exercises/untitled/src/Exercise6/FindingMissingInt.java
@@ -28,6 +28,11 @@ public class FindingMissingInt {
     }
 
     public static Integer findMissingNumber(Integer[] arr) {
-        
+        for (int i = 0; i < arr.length; i++) {
+            if ((arr[i] + 1) != arr[(i + 1)]) {
+                return arr[(i + 1)];
+            }
+        }
+        return null;
     }
 }
