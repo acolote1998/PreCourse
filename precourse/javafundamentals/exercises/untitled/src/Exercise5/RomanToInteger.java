1717685d152006d2250e0182c3b2aab146da6c72
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-02-28 08:51:42 +0100

    Exercise 5 - Roman to Integer

---
+++ precourse/javafundamentals/exercises/untitled/src/Exercise5/RomanToInteger.java
@@ -0,0 +1,20 @@
+package Exercise5;
+
+/*
+
+🚀 Exercise 5: Roman to Integer
+Problem Description
+Roman numerals are used to represent integer values with specific symbols. Your task is to write a function that converts a given Roman numeral into its corresponding integer value.
+
+Roman numerals are read from left to right, but there's a twist:
+
+If a smaller-value symbol appears before a larger-value symbol, it is subtracted.
+If a smaller-value symbol appears after a larger-value symbol, it is added.
+
+ */
+
+public class RomanToInteger {
+    public static void main(String[] args) {
+
+    }
+}
