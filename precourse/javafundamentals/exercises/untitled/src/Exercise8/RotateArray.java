e397e02b4fc7d5b1ee786787156a388a311f1388
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-03-01 20:26:16 +0100

    Exercise 8 - initialisation

---
+++ precourse/javafundamentals/exercises/untitled/src/Exercise8/RotateArray.java
@@ -0,0 +1,35 @@
+package Exercise8;
+
+/*
+
+🚀 Exercise: Rotate an Array
+Problem Description:
+You are given an array arr[] of n elements and an integer k. Write a function to rotate the array arr[] to the right by k steps. The rotation should be done in-place.
+
+Function Signature:
+
+public static void rotateArray(int[] arr, int k);
+
+Input:
+An array of integers arr[] (1 <= arr.length <= 10^4).
+An integer k (0 <= k <= arr.length).
+
+Output:
+The array rotated by k steps to the right.
+
+Example:
+System.out.println(Arrays.toString(rotateArray(new int[]{1, 2, 3, 4, 5}, 2))); // Expected Output: [4, 5, 1, 2, 3]
+System.out.println(Arrays.toString(rotateArray(new int[]{7, 8, 9}, 1))); // Expected Output: [9, 7, 8]
+System.out.println(Arrays.toString(rotateArray(new int[]{1, 2}, 3))); // Expected Output: [2, 1]
+
+Additional Considerations:
+You can assume k is always a valid input (i.e., between 0 and the length of the array).
+Think of an efficient way to perform the rotation without creating a new array (i.e., do it in-place).
+
+ */
+
+public class RotateArray {
+    public static void main(String[] args) {
+
+    }
+}
