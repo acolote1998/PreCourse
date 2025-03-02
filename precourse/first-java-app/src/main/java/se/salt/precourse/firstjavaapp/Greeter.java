a992eb0f9f83f75143a78672cf2c3926eafdc2b9
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-03-02 15:55:39 +0100

    spring boot initialization

--- precourse/first-java-app/src/main/java/se/salt/precourse/firstjavaapp/Greeter.java
+++ precourse/first-java-app/src/main/java/se/salt/precourse/firstjavaapp/Greeter.java
@@ -6,31 +6,19 @@ import java.io.InputStreamReader;
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-public class Greeter {
-  private static String greet(String namePassedIn) {
-    return "Welcome to SALT, " + namePassedIn;
+@SpringBootApplication
+public class Greeter implements CommandLineRunner {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Greeter.class, args);
   }
 
-  private static String calculateDate(LocalDate courseStartDate){
-    LocalDate actualDate = LocalDate.now();
-
-    long daysLeft = ChronoUnit.DAYS.between(actualDate,courseStartDate);
-
-    return "There are "+daysLeft+" days left until the course starts.";
-  }
-
-  private static boolean dateHasOnlyNumbers(String startDateInput){
-    String[] dateparts = startDateInput.split("-");
-    for(String datePart : dateparts){
-      if (!StringUtils.isNumeric(datePart)){
-        return false;
-      }
-    }
-    return true;
-  }
-
-  public static void main(String[] args) throws IOException {
+  @Override
+  public void run(String... args) throws IOException {
     System.out.print("What is your name? ");
     BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
     String name = reader.readLine();
@@ -48,7 +36,25 @@ public class Greeter {
     } else {
       System.out.println("Please provide a valid date");
     }
+  }
 
+  private static String greet(String namePassedIn) {
+    return "Welcome to SALT, " + namePassedIn;
+  }
+
+  private static String calculateDate(LocalDate courseStartDate) {
+    LocalDate actualDate = LocalDate.now();
+    long daysLeft = ChronoUnit.DAYS.between(actualDate, courseStartDate);
+    return "There are " + daysLeft + " days left until the course starts.";
+  }
+
+  private static boolean dateHasOnlyNumbers(String startDateInput) {
+    String[] dateParts = startDateInput.split("-");
+    for (String datePart : dateParts) {
+      if (!StringUtils.isNumeric(datePart)) {
+        return false;
+      }
+    }
+    return true;
   }
 }
-
