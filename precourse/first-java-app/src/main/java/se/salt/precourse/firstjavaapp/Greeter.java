5709d141d26bfd510248892a574b48becfd5c155
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-03-02 15:35:12 +0100

    date validation with plugin

--- precourse/first-java-app/src/main/java/se/salt/precourse/firstjavaapp/Greeter.java
+++ precourse/first-java-app/src/main/java/se/salt/precourse/firstjavaapp/Greeter.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
+import org.apache.commons.lang3.StringUtils;
 
 public class Greeter {
   private static String greet(String namePassedIn) {
@@ -19,22 +20,35 @@ public class Greeter {
     return "There are "+daysLeft+" days left until the course starts.";
   }
 
+  private static boolean dateHasOnlyNumbers(String startDateInput){
+    String[] dateparts = startDateInput.split("-");
+    for(String datePart : dateparts){
+      if (!StringUtils.isNumeric(datePart)){
+        return false;
+      }
+    }
+    return true;
+  }
+
   public static void main(String[] args) throws IOException {
     System.out.print("What is your name? ");
-
     BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
     String name = reader.readLine();
 
-    System.out.print("When does the course start?");
-
-    LocalDate courseStart = LocalDate.parse(reader.readLine());
+    System.out.print("When does the course start? ");
+    String startDateInput = reader.readLine();
 
     String greeting = greet(name);
-
     System.out.println(greeting);
 
-    String calculatingDaysLeft = calculateDate(courseStart);
+    if (dateHasOnlyNumbers(startDateInput)) {
+      LocalDate courseStart = LocalDate.parse(startDateInput);
+      String calculatingDaysLeft = calculateDate(courseStart);
+      System.out.println(calculatingDaysLeft);
+    } else {
+      System.out.println("Please provide a valid date");
+    }
 
-    System.out.println(calculatingDaysLeft);
   }
 }
+
