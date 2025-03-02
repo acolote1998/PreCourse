6413a67a912ccf1d20545644cb64bc0a505813bc
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-03-02 15:46:58 +0100

    testing class

---
+++ precourse/first-java-app/src/test/java/se/salt/precourse/firstjavaapp/GreeterTest.java
@@ -0,0 +1,12 @@
+package se.salt.precourse.firstjavaapp;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class GreeterTest {
+
+}
