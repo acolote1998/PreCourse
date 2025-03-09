f833433ac7e285e177cd8ebe6d178cbe713ef236
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-03-09 18:18:11 +0100

    Another interface example

--- precourse/typescript-trainingGround/dist/index.js
+++ precourse/typescript-trainingGround/dist/index.js
@@ -1,8 +1,69 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.EmployeeClass = exports.PersonClass = void 0;
 exports.greet = greet;
+exports.isOld = isOld;
+exports.countOdd = countOdd;
+exports.sumEvenNumbers = sumEvenNumbers;
+exports.getPersonStreetNo = getPersonStreetNo;
+exports.getPersonNameString = getPersonNameString;
 function greet(name, birthYear) {
     const age = new Date().getFullYear() - birthYear;
     return `Hello ${name}, you are ${age} years old`;
+}
+function isOld(age) {
+    return age >= 35;
+}
+function countOdd(arr) {
+    let oddnumber = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] % 2 > 0) {
+            oddnumber++;
+        }
+    }
+    return oddnumber;
+}
+function sumEvenNumbers(arr) {
+    let summedNumbers = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] % 2 == 0) {
+            summedNumbers += arr[i];
+        }
+    }
+    return summedNumbers;
+}
+function getPersonStreetNo(person) {
+    return person.address.streetNo;
+}
+class PersonClass {
+    getName() {
+        return this.name;
+    }
+    getBirthYear() {
+        return this.birthYear;
+    }
+    constructor(name, brithYear) {
+        this.name = "";
+        this.birthYear = 0;
+        this.name = name;
+        this.birthYear = this.birthYear;
+    }
+}
+exports.PersonClass = PersonClass;
+class EmployeeClass extends PersonClass {
+    constructor() {
+        super(...arguments);
+        this.employeeId = -1;
+    }
+    getEmployeeId() {
+        return this.employeeId;
+    }
+    setEmployeeId(id) {
+        this.employeeId = id;
+    }
+}
+exports.EmployeeClass = EmployeeClass;
+function getPersonNameString(p) {
+    return `${p.name}, ${p.birthYear}`;
 }
 //# sourceMappingURL=index.js.map
--- precourse/typescript-trainingGround/dist/index.test.js
+++ precourse/typescript-trainingGround/dist/index.test.js
@@ -17,4 +17,81 @@ describe("ts test", () => {
         assert_1.default.strictEqual(result, "Hello Marcus, you are 53 years old");
     });
 });
+it("returns true if age is above or equal to 35", () => {
+    // act
+    const is34Old = (0, index_1.isOld)(34);
+    const is35Old = (0, index_1.isOld)(35);
+    const is36Old = (0, index_1.isOld)(36);
+    // assert
+    assert_1.default.strictEqual(is34Old, false);
+    assert_1.default.strictEqual(is35Old, true);
+    assert_1.default.strictEqual(is36Old, true);
+});
+it("fun with variables", () => {
+    //act
+    let name = "Marcus";
+    let nameImplicit = "Marcus";
+    let nameImplicit2 = name;
+    const cool = true;
+    const birthYear = 1972;
+    //arrange
+    assert_1.default.strictEqual(typeof cool, "boolean");
+    assert_1.default.strictEqual(typeof birthYear, "number");
+    assert_1.default.strictEqual(typeof nameImplicit, "string");
+    assert_1.default.strictEqual(typeof nameImplicit2, "string");
+    assert_1.default.strictEqual(typeof name, "string");
+});
+it("count odd numbers", () => {
+    //arrange
+    const firstFive = [1, 2, 3, 4, 5];
+    //act
+    const numberOfOdds = (0, index_1.countOdd)(firstFive);
+    //arrange
+    assert_1.default.strictEqual(numberOfOdds, 3);
+});
+it("sums even numbers", () => {
+    //arrange
+    const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    //actr
+    const testsumEvenNumbers = (0, index_1.sumEvenNumbers)(nums);
+    //arrange
+    assert_1.default.strictEqual(testsumEvenNumbers, 30);
+});
+it("gets the street number for a person", () => {
+    //arrange
+    const p = {
+        name: "Marcus",
+        birthYear: 1972,
+        address: {
+            street: "Solnatest",
+            streetNo: 25,
+            city: "Stockholm",
+        },
+    };
+    //act
+    const streetN = (0, index_1.getPersonStreetNo)(p);
+    // assert
+    assert_1.default.strictEqual(streetN, 25);
+});
+it("using classes", () => {
+    const p = new index_1.PersonClass("Marcus", 1972);
+    const e = new index_1.EmployeeClass("Marcus Employee", 1972);
+    //act
+    e.setEmployeeId(12345);
+    //assert
+    assert_1.default.strictEqual(p.getName(), "Marcus");
+    assert_1.default.strictEqual(e.getName(), "Marcus Employee");
+    assert_1.default.strictEqual(e.getEmployeeId(), 12345);
+});
+it("prints an IPerson", () => {
+    //arrange
+    const p1 = { name: "Marcus", birthYear: 1972 };
+    const p2 = { name: "David", birthYear: 1975, drummer: true };
+    //act
+    const p1Address = (0, index_1.getPersonNameString)(p1);
+    const p2Address = (0, index_1.getPersonNameString)(p2);
+    //assert
+    assert_1.default.strictEqual(p1Address, "Marcus, 1972");
+    assert_1.default.strictEqual(p2Address, "David, 1975");
+});
 //# sourceMappingURL=index.test.js.map
