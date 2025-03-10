29e72516e961b680bd94db4eee43f9207e63d995
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-03-10 22:45:53 +0100

    adding props to the component

--- React/firstReactApp/firstApp/src/App.jsx
+++ React/firstReactApp/firstApp/src/App.jsx
@@ -7,13 +7,16 @@ import MyButton from "./components/MyButton";
 import ShoppingList from "./components/ShoppingList";
 
 function App() {
+  const [count, setCount] = useState(0);
+  function handleClick() {
+    setCount(count + 1);
+  }
   return (
     <>
       <div>
         <h1>Welcome to my app</h1>
-        <MyButton />
-        <MyButton />
-        <MyButton />
+        <MyButton count={count} onClick={handleClick} />
+        <MyButton count={count} onClick={handleClick} />
       </div>
       <div>
         <ShoppingList />
