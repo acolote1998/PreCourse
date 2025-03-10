8dedbd9ed4d970c6604d987b03d9f16f6187fcb8
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-03-10 15:28:04 +0100

    Using JS in a component

--- React/firstReactApp/firstApp/src/components/MyButton.jsx
+++ React/firstReactApp/firstApp/src/components/MyButton.jsx
@@ -1,5 +1,12 @@
 function MyButton() {
-  return <button>I'm a button</button>;
+  const name = prompt("What is your name?");
+  let content;
+  if (name == "aki") {
+    content = "You are the best";
+  } else {
+    content = "Uhm... hi...";
+  }
+  return <h1>{content}</h1>;
 }
 
 export default MyButton;
