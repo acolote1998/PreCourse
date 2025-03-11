75d944775ebb16d2eef16e4651587dbb489cafb0
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-03-11 17:45:34 +0100

    initialising second API project

---
+++ second-rest-api/src/main/java/com/example/second_rest_api/model/MutableLiveStream.java
@@ -0,0 +1,46 @@
+package com.example.second_rest_api.model;
+
+import java.time.LocalDateTime;
+
+public class MutableLiveStream {
+
+    private String id;
+    private String title;
+    private String description;
+    private String url;
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public LocalDateTime getStartDate() {
+        return startDate;
+    }
+
+    public LocalDateTime getEndDate() {
+        return endDate;
+    }
+
+    public MutableLiveStream(String id, String title, String description, String url, LocalDateTime startDate, LocalDateTime endDate) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.url = url;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+}
---
+++ second-rest-api/src/test/java/com/example/second_rest_api/SecondRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.second_rest_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
