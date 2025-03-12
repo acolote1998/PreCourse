a6a5d8ed65e44873f0fc3ea85ebc0337d0f1a6d5
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-03-12 10:06:22 +0100

    LiveStream repository class

--- second-rest-api/src/main/java/com/example/second_rest_api/controller/LiveStreamController.java
+++ second-rest-api/src/main/java/com/example/second_rest_api/controller/LiveStreamController.java
@@ -1,16 +1,19 @@
 package com.example.second_rest_api.controller;
 
 import com.example.second_rest_api.model.LiveStream;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/streams")
 public class LiveStreamController {
 
-    // http://localhost.com/streams
+    // GET http://localhost.com/streams
+    @GetMapping
     public List<LiveStream> findAll(){
      return null;
     }
