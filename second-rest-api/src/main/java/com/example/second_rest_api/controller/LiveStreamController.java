34b74737ed6be7dafc6f8f8efdc596d0ed034c55
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-03-12 10:34:24 +0100

    Feat: creating new streams through the controller

--- second-rest-api/src/main/java/com/example/second_rest_api/controller/LiveStreamController.java
+++ second-rest-api/src/main/java/com/example/second_rest_api/controller/LiveStreamController.java
@@ -2,10 +2,8 @@ package com.example.second_rest_api.controller;
 
 import com.example.second_rest_api.model.LiveStream;
 import com.example.second_rest_api.repository.LiveStreamRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -26,9 +24,17 @@ public class LiveStreamController {
     public List<LiveStream> findAll(){
      return repository.findAll();
     }
+
     // GET http://localhost.com/streams/123123-123-123-123(example)
     @GetMapping("/{id}")
     public LiveStream findById(@PathVariable String id){
         return repository.findById(id);
+    }
+
+    // POST http://localhost:8080/streams
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    public LiveStream create (@RequestBody LiveStream stream){
+        return repository.create(stream);
     }
 }
