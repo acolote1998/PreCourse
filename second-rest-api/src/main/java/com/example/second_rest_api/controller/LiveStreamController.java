98f376f59940c6752bd4c2c35b78c91b60cad434
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-03-12 10:25:39 +0100

    Controller method: find stream by id

--- second-rest-api/src/main/java/com/example/second_rest_api/controller/LiveStreamController.java
+++ second-rest-api/src/main/java/com/example/second_rest_api/controller/LiveStreamController.java
@@ -3,6 +3,7 @@ package com.example.second_rest_api.controller;
 import com.example.second_rest_api.model.LiveStream;
 import com.example.second_rest_api.repository.LiveStreamRepository;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,5 +25,10 @@ public class LiveStreamController {
     @GetMapping
     public List<LiveStream> findAll(){
      return repository.findAll();
+    }
+    // GET http://localhost.com/streams/123123-123-123-123(example)
+    @GetMapping("/{id}")
+    public LiveStream findById(@PathVariable String id){
+        return repository.findById(id);
     }
 }
